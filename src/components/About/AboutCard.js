73dc056cbe875015f9a75d22ef911f8f5963c858
Susilkessav <56352025+Susilkessav@users.noreply.github.com>
2024-09-29 14:11:09 -0400

    changes in about

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,11 +9,18 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Susilkessav </span>
-            from <span className="purple"> India.</span>
+
+            
             <br />
             I hold a Master’s in Data Analytics Engineering from Northeastern University.
+            <br/>
+            My expertise lies in building robust data pipelines, integrating machine learning models, and developing efficient solutions for data-driven problems.
             <br />
-            I have a strong passion for data engineering, robotics, and solving complex real-world problems with machine learning.
+            I’ve worked on projects involving real-time data processing, predictive analytics, and applying machine learning algorithms to complex datasets.
+            <br />
+            I enjoy tackling challenges like optimizing data workflows, improving model performance, and creating user-friendly interfaces for visualizing data insights. I’m passionate about leveraging data and machine learning to deliver impactful solutions that drive innovation.
+            <br />
+            I have a strong passion for data engineering, and solving complex real-world problems with machine learning.
             <br />
             <br />
             Besides coding, here are a few activities that I enjoy!
